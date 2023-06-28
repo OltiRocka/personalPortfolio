166a5c2c856514be7f79ff924202e8639ea06067
Olti Roka <rocka@pop-os.localdomain>
2023-06-28 16:37:38 +0200

    fixed:first-commit

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,44 +1,44 @@
-import React from 'react'
-import rust from '../assets/skills/rust.png'
-import python from '../assets/skills/python.svg'
-import machLea from '../assets/skills/brainstorming.png'
-import webSc from '../assets/skills/data-mining.png'
-import visualize from '../assets/skills/bar-chart.png'
-import api from '../assets/skills/api.png'
-import auto from '../assets/skills/bot.png'
-import genAi from '../assets/skills/artificial-intelligence.png'
-import './skills.css'
+import React from "react";
+import machLea from "../assets/skills/brainstorming.png";
+import webSc from "../assets/skills/data-mining.png";
+import visualize from "../assets/skills/bar-chart.png";
+import api from "../assets/skills/api.png";
+import auto from "../assets/skills/bot.png";
+import genAi from "../assets/skills/artificial-intelligence.png";
+import "./skills.css";
 const Skills = () => {
   const tools = [
     // { img: python, name: 'Python' },
     // { img: rust, name: 'Rust' },
-    { img: machLea, name: 'Machine Learning' },
-    { img: webSc, name: 'WebScraping' },
-      { img: visualize, name: 'Data Visualising' },
-      { img: api, name: 'APIs' },
-      { img: auto, name: 'Automation' },
-      { img: genAi, name: 'Generative AI' }
-  ]
+    { img: machLea, name: "Machine Learning", description: "TensorFlow or PyTorch for problem solutions." },
+    { img: webSc, name: "Data Analytics", description: "Pandas, SciKit-Learn, NumPy." },
+    { img: visualize, name: "Data Visualising", description: "PowerBI and Tableau." },
+    { img: api, name: "Backend Engineering", description: "FastAPI or Flask to develop backend solutions." },
+    { img: auto, name: "Automation", description: "Selenium, BeautifulSoup, RobotFramework, PyautoGUI." },
+    { img: genAi, name: "Generative AI", description: "Diffusers Framework and Diffusers Models." },
+  ];
   return (
     <div className="tools">
-    <h2 className="text">Services</h2>
-    <div className="tool-container">
-        {tools.slice(0,3).map((tool, index) => (
-            <div key={index} className="tool">
-              <div className="image-container">
-                <img src={tool.img} alt={tool.name} />
-              </div>
-              <p>{tool.name}</p>
+      <p className="text">My Services</p>
+      <div className="tool-container">
+        {tools.slice(0, 3).map((tool, index) => (
+          <div key={index} className="tool">
+            <div className="image-container">
+              <img src={tool.img} alt={tool.name} />
             </div>
+            <p className="name-service">{tool.name}</p>
+            <p className="description-service">{tool.description}</p>
+          </div>
         ))}
-</div>
+      </div>
       <div className="tool-container">
         {tools.slice(-3).map((tool, index) => (
-            <div key={index} className="tool">
-              <div className="image-container">
-                <img src={tool.img} alt={tool.name} />
-              </div>
-            <p>{tool.name}</p>
+          <div key={index} className="tool">
+            <div className="image-container">
+              <img src={tool.img} alt={tool.name} />
+            </div>
+            <p className="name-service">{tool.name}</p>
+            <p className="description-service">{tool.description}</p>
           </div>
         ))}
       </div>
